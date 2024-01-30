c085b7db93d62e6444d5f72aa2988555c0400b13
elkocho1 <>
2024-01-30 20:46:26 +0000

    Add and print  game rules

--- run.py
+++ run.py
@@ -3,3 +3,12 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 import random
+
+"""
+ Battleship game
+"""
+
+print("Welcome to my Battleship game")
+print("Board Size ist 10 x 10 and each player has 8 ships with a max size of 4 hits.")
+print("You have 50 bullets to take down the enemy ships. Enjoy the game!")
+
