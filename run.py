9131044fb29bbc422f67fae857cfba452f42839b
elkocho1 <>
2024-01-30 21:03:44 +0000

    Add global variables

--- run.py
+++ run.py
@@ -7,9 +7,21 @@ import random
 """
  Battleship game
 """
+
+# Global variables
+board = [[]]
+board.size = 10
+num_of_ships = 2
+bullets_left = 50
+game_over = False
+num_of_ships_sunk = 0
+ship_positions = [[]]
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
 def play():
     print("Welcome to my Battleship game")
-    print("Board Size ist 10 x 10 and each player has 8 ships with a max size of 4 hits.")
+    print("Board Size ist 10 x 10 and each player has 8 ships.")
     print("You have in total 50 bullets to take down the enemy ships. Each round the amount will be updated and the hits and misses are getting displayed.")
 
 play()
